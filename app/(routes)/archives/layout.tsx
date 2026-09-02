4d96382ef9f527d30433e0c41c42491321accdca
ininahazwe <yves.cri@gmail.com>
2026-09-02 10:52:32 +0000

    remove highlights from archive page

--- app/(routes)/archives/layout.tsx
+++ app/(routes)/archives/layout.tsx
@@ -19,7 +19,6 @@ export default async function ArchivesLayout({ children }: { children: React.Rea
         <>
             <Header />
 
-            <SiteBanner articles={bannerArticles} categories={bannerCategories} />
 
             {children}
 
